1f6faa0eacb433c93ab239caf341478eddc929b6
Wujie <619685446@qq.com>
2016-09-24 16:31:48 +0800

    2016.9.24c.c F1

---
+++ 2016.9.24c.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#define N 40
+int readscore(int score[]);
+void printscore(int score[],int n);
+void asc(int a[],int n);
+void des(int a[],int n);
+void swap(int *x,int *y);
+int main()
+{
+    int score[N],n;
+    int order;
+    n=readscore[score];
+    printf("total students are %d\n",n);
+    printf("enter 1 to sort in asc order\n");
+    printf("enter 2 to sort in des order\n");
+    printf("data item sort in orignal order:\n");
+    printscore[score,n];
+    if(order==1)
+    {
+        printf("data item sort in asc order\n");
+	asc(score,n);
+    }
+    else
+    {
+        printf("data item sort in des oeder\n");
+	des(score,n);
+    }
+    printscore(score,n);
+}
+int readscore(int score[],int n)
+{
+    int i;
+    i=-1;
+    do{
+        i++;
+	printf("put your the %d score",i+1);
+	scanf("%d",&score[i]);
+    }while(score[i]>0);
+    return i;
+}
+void printscore(int score[],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+        printf("%-5d",&score[i]);
+    }
+    printf("\n");
+}
+void asc(int a[],int n)
+{
+    int i,j,k;
+    for(i=0;i<n-1;i++)
+    {
+        k=i;
+        for(j=i+1;j<n;j++)
+	{
+	    if(score[j]<score[k]) k=j;
+	}
+	if(k!=i) swap(&a[k],&a[i]);
+    }
+}
+void des(int a[],int n)
+{
+    int i,j,k;
+    for(i=0;i<n-1;i++)
+    {
+        k=i;
+	for(j=i+1;j<n;j++)
+	{
+	    if(score[j]>score[k]) k=j;
+	}
+	if(k!=i) swap(&a[k],&a[i]);
+    }
+}
+void swap(int *x,int *y)
+{
+ int *temp;
+ temp=*x;
+ *x=*y;
+ *y=temp;
+}
+
+
+
+
+
